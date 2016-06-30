873f2c72495957d64122d4fae4490f7585c023b0
claudiuvertistudio <claudiu.buruiana@codeinwp.com>
2016-06-30 12:10:56 +0300

    Upload child theme

---
+++ content-single.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @package llorix-one-lite
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class('content-single-page'); ?>>
+
+		<header class="entry-header single-header">
+			<div class="single-header-overlay"></div>
+			<?php
+				if ( has_post_thumbnail() ) {
+					the_post_thumbnail();
+				}
+			?>
+			<div class="inner-title-wrap">
+				<div class="container">
+					<div>
+					<?php the_title( '<h1 itemprop="headline" class="entry-title single-title">', '</h1>' ); ?>
+					<div class="entry-meta single-entry-meta">
+						<span class="author-link" itemprop="author" itemscope="" itemtype="http://schema.org/Person">
+							<span itemprop="name" class="post-author author vcard">
+								<a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' )); ?>" itemprop="url" rel="author"><?php the_author(); ?></a>
+							</span>
+			            </span>
+						<time class="post-time posted-on published" datetime="<?php the_time('c'); ?>" itemprop="datePublished">
+							<?php the_time( get_option('date_format') ); ?>
+						</time>
+					</div><!-- .entry-meta -->
+				</div>
+				</div>
+			</div>
+		</header><!-- .entry-header -->
+
+	<div class="container">
+		<div class="inner-content-wrap">
+			<div itemprop="text" class="entry-content">
+				<?php the_content(); ?>
+				<?php
+					wp_link_pages( array(
+						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'llorix-one-lite' ),
+						'after'  => '</div>',
+					) );
+				?>
+			</div><!-- .entry-content -->
+
+			<footer class="entry-footer">
+				<?php llorix_one_lite_entry_footer(); ?>
+			</footer><!-- .entry-footer -->
+		</div>
+	</div>
+
+</article><!-- #post-## -->
---
+++ functions.php
@@ -0,0 +1,85 @@
+<?php
+
+function nature_enqueue_styles() {
+
+    $parent_style = 'nature-parent-style';
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'nature-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ));
+
+    wp_enqueue_style( 'nature-fonts', nature_fonts_url(), array(), null );
+
+}
+add_action( 'wp_enqueue_scripts', 'nature_enqueue_styles' );
+
+if ( get_stylesheet() !== get_template() ) {
+    add_filter( 'pre_update_option_theme_mods_' . get_stylesheet(), function ( $value, $old_value ) {
+         update_option( 'theme_mods_' . get_template(), $value );
+         return $old_value; // prevent update to child theme mods
+    }, 10, 2 );
+    add_filter( 'pre_option_theme_mods_' . get_stylesheet(), function ( $default ) {
+        return get_option( 'theme_mods_' . get_template(), $default );
+    } );
+}
+
+
+/**
+ * Return the Google font stylesheet URL, if available.
+ *
+ * The use of Source Sans Pro and Bitter by default is localized. For languages
+ * that use characters not supported by the font, the font can be disabled.
+ *
+ * @since InMotion 1.0
+ *
+ * @return string Font stylesheet or empty string if disabled.
+ */
+function nature_fonts_url() {
+    $fonts_url = '';
+    /* Translators: If there are characters in your language that are not
+     * supported by Bitter, translate this to 'off'. Do not translate into your
+     * own language.
+     */
+    $bitter = _x( 'on', 'Cabin font: on or off', 'zillah' );
+    if ( 'off' !== $bitter ) {
+        $font_families = array();
+        if ( 'off' !== $bitter )
+            $font_families[] = 'Bitter:400,400i';
+        $query_args = array(
+            'family' => urlencode( implode( '|', $font_families ) ),
+            'subset' => urlencode( 'latin,latin-ext' ),
+        );
+        $fonts_url = add_query_arg( $query_args, '//fonts.googleapis.com/css' );
+    }
+    return $fonts_url;
+}
+
+
+if ( ! function_exists( 'nature_entry_date' ) ) :
+	/**
+	 * Prints HTML with date information for current post.
+	 *
+	 * Create your own twentysixteen_entry_date() function to override in a child theme.
+	 *
+	 * @since Twenty Sixteen 1.0
+	 */
+	function nature_entry_date() {
+		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+
+		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+		}
+
+		$time_string = sprintf( $time_string,
+			esc_attr( get_the_date( 'c' ) ),
+			get_the_date(),
+			esc_attr( get_the_modified_date( 'c' ) ),
+			get_the_modified_date()
+		);
+
+		printf( '<span class="posted-on"><span class="screen-reader-text">%1$s </span><a href="%2$s" rel="bookmark">%3$s</a></span>',
+			_x( 'Posted on', 'Used before publish date.', 'twentysixteen' ),
+			esc_url( get_permalink() ),
+			$time_string
+		);
+
+	}
+endif;
